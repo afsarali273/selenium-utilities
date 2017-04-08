42d938eeeecd7695d42078f9810f7895acde0b18
Md Afsar Ali <afsarali273@gmail.com>
2017-04-09 01:44:26 +0530

    Update TestNGRunner.java

--- src/main/java/TestNGRunner.java
+++ src/main/java/TestNGRunner.java
@@ -1,28 +1,11 @@
 package runner;
-
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Hashtable;
 import java.util.List;
-import java.util.Map;
-
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.testng.TestNG;
-import org.testng.annotations.Test;
 import org.testng.xml.XmlClass;
 import org.testng.xml.XmlInclude;
 import org.testng.xml.XmlSuite;
 import org.testng.xml.XmlTest;
-
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
 import com.test.Class1;
 import com.test.Class2;
 import com.test.Class3;
@@ -32,15 +15,11 @@ import com.test.Class5;
 import utility.ExcelUtils;
 
 public class demo {
-	Multimap<String, String> map;
 	String TestName;
 	static List<String> methodname;
-	static HSSFWorkbook wb;
-	static HSSFSheet sheet;
-	static HSSFCell cell;
-	static HSSFRow row;
 	static XmlTest mainTest;
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws Exception {
 
 		ExcelUtils.setExcelFile("E:\\EclipseTests\\DemoTests\\src\\main\\java\\testdata\\testdata.xls", "suite");
@@ -49,6 +28,7 @@ public class demo {
 		mainsuite.setName("SmokeTest Suite");
 		mainsuite.setFileName("SmokeTest.xml");
 		mainsuite.setParallel("false");
+		mainsuite.addListener("utility.Listeners");
 
 		for(int rowIndex=1;rowIndex<=ic;rowIndex++){
 
@@ -59,8 +39,7 @@ public class demo {
 
 				String tcname=ExcelUtils.getCellData(rowIndex, 0);
 				System.out.println("tc name : "+tcname+"  : "+run ); 
-
-				XmlTest xmlTest=getTest(tcname, mainsuite);
+				getTest(tcname, mainsuite);
 			}
 		}
 
@@ -74,57 +53,40 @@ public class demo {
 	public static XmlTest getTest(String TCName,XmlSuite mainsuite) throws Exception {
 
 		XmlClass classlist = null;
-		List<XmlInclude>xmlInclude=null;
 		XmlTest mainTest=null;
 		methodname=null;
 
 		switch (TCName) {
 
 		case "Class1":	
+			
 			classlist = new XmlClass(Class1.class);
-			//XmlTest mainTest;
-			mainTest = new XmlTest(mainsuite);
-			mainTest.setName(TCName);
-			mainTest.setPreserveOrder("True");
-			methodname=methods(TCName);	
-			for(String a: methodname){
+			mainTest=xmltest(mainsuite, TCName);
+			for(String a: methods(TCName)){
 				classlist.getIncludedMethods().add(new XmlInclude(a));
-
 			}		
 			break;
 
 		case "Class2":
 
 			classlist = new XmlClass(Class2.class);
-			mainTest = new XmlTest(mainsuite);
-			mainTest.setName(TCName);
-			mainTest.setPreserveOrder("True");
-			methodname=methods(TCName);
-
-			for(String a: methodname){
+			mainTest=xmltest(mainsuite, TCName);
+			for(String a: methods(TCName)){
 				classlist.getIncludedMethods().add(new XmlInclude(a));
 			}
 			break;
 
 		case "Class3":
 			classlist = new XmlClass(Class3.class);
-			mainTest = new XmlTest(mainsuite);
-			mainTest.setName(TCName);
-			mainTest.setPreserveOrder("True");
-			methodname=methods(TCName);
-
-			for(String a: methodname){
+			mainTest=xmltest(mainsuite, TCName);
+			for(String a: methods(TCName)){
 				classlist.getIncludedMethods().add(new XmlInclude(a));
 			}
 			break;
 		case "Class4":
 			classlist = new XmlClass(Class4.class);
-			mainTest = new XmlTest(mainsuite);
-			mainTest.setName(TCName);
-			mainTest.setPreserveOrder("True");
-			methodname=methods(TCName);
-
-			for(String a: methodname){
+			mainTest=xmltest(mainsuite, TCName);
+			for(String a: methods(TCName)){
 				classlist.getIncludedMethods().add(new XmlInclude(a));
 			}
 			break;
@@ -162,5 +124,11 @@ public class demo {
 		return method;
 	}
 
-
+	public static XmlTest xmltest(XmlSuite mainsuite ,String TCName){
+		XmlTest mainTest=null;
+		mainTest = new XmlTest(mainsuite);
+		mainTest.setName(TCName);
+		mainTest.setPreserveOrder("True");
+		return mainTest;
+	}
 }
